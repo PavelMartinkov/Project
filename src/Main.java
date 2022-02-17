6e8fb9717d66aa58166ad4a5469ee8472f660919
Pavel Martinkov <pavel.martinkov98@mail.ru>
2022-02-17 22:45:43 +0100

    commit3

---
+++ src/Main.java
@@ -0,0 +1,36 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int vasilAge = 30;
+        int kateAge = 17;
+        int mishaAge = 17;
+
+        int min = -1;
+        int middle = -1;
+        int max = -1;
+
+        if (vasilAge <= kateAge && vasilAge <= mishaAge) {
+            System.out.println("Minimal age: " + vasilAge);
+        } else if (vasilAge <= kateAge && vasilAge >= mishaAge) {
+            System.out.println("Minimal age: " + mishaAge);
+        } else if (vasilAge >= kateAge && kateAge <= mishaAge) {
+            System.out.println("Minimal age: " + kateAge);
+        }
+
+        if (vasilAge >= kateAge && kateAge >= mishaAge) {
+            System.out.println("Middle age: " + kateAge);
+        } else if (vasilAge >= kateAge && mishaAge >= kateAge) {
+            System.out.println("Middle age: " + vasilAge);
+        } else if (vasilAge <= kateAge && mishaAge <= kateAge) {
+            System.out.println("Middle age: " + mishaAge);
+        }
+
+        if (vasilAge >= kateAge && vasilAge >= mishaAge) {
+            System.out.println("Maximum age: " + vasilAge);
+        } else if (vasilAge >= kateAge && vasilAge <= mishaAge) {
+            System.out.println("Maximum age: " +  mishaAge);
+        } else if (vasilAge <= kateAge && kateAge >= mishaAge) {
+            System.out.println("Maximum age: " +  kateAge);
+        }
+    }
+}
